7c0abf1b0ad4812910da4872e25fcba0c741ab5b
ranayuvrajsingh <ranayuvrajsingh0000@gmail.com>
2023-10-24 17:40:09 +0530

    initial

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import Navbar from "@/components/Navbar";
+import "./globals.css";
+import type { Metadata } from "next";
+import Footer from "@/components/Footer";
+
+export const metadata: Metadata = {
+  title: "iShowSpeed",
+  description: "Speed Thrills but Safety is Important",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="relative">
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
